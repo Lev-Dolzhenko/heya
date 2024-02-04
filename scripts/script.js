56c9d1f52f66a771b769b438b589f49a9f2aaad2
Leo <workspace2707@yandex.ru>
2024-02-04 13:06:26 +0300

    created all, footer blocks

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
+// DATE FOR COPYRIGHT
+
+const currDate = new Date()
+const copyRightText = document.getElementById('copyrightYear')
+
+copyRightText.textContent = currDate.getFullYear()
+
+
 var swiper = new Swiper(".mySwiper", {
+  loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".case-button-next",
+    prevEl: ".case-button-prev",
   },
 });

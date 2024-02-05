74bbf95947e3f838f4c75076576603c6329af1d3
Leo <workspace2707@yandex.ru>
2024-02-05 11:27:24 +0300

    created scripts for tags, created mobile nav, fixed button of slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
 // DATE FOR COPYRIGHT
 
-const currDate = new Date()
-const copyRightText = document.getElementById('copyrightYear')
+const currDate = new Date();
+const copyRightText = document.getElementById("copyrightYear");
 
-copyRightText.textContent = currDate.getFullYear()
-
+copyRightText.textContent = currDate.getFullYear();
 
 var swiper = new Swiper(".mySwiper", {
   loop: true,
@@ -12,4 +11,71 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".case-button-next",
     prevEl: ".case-button-prev",
   },
+});
+
+// ADD TAGS TO SEARCH
+
+const tags = document.querySelectorAll(".header__tags_item");
+const searchTags = document.querySelectorAll(".form__wrapper_tag");
+const closeTags = document.querySelectorAll(".button_tag");
+const screenWidth = window.screen.width;
+
+for (let tag of tags) {
+  tag.addEventListener("click", function () {
+    const tagAtr = tag.getAttribute("data-tag");
+    for (let searchTag of searchTags) {
+      const searchTagAtr = searchTag.getAttribute("data-search-tag");
+      if (tagAtr === searchTagAtr) {
+        if (screenWidth >= 680) {
+          searchTag.classList.remove("none");
+        } else if (screenWidth < 680) {
+          tag.classList.toggle("active");
+        }
+      }
+    }
+  });
+}
+
+for (let closeTag of closeTags) {
+  const closeTagAtr = closeTag.getAttribute("data-close");
+  closeTag.addEventListener("click", function (e) {
+    e.preventDefault();
+    for (let searchTag of searchTags) {
+      const searchTagAtr = searchTag.getAttribute("data-search-tag");
+      if (closeTagAtr === searchTagAtr) {
+        searchTag.classList.add("none");
+      }
+    }
+  });
+}
+
+// DROPDOWN
+
+const dropdowns = document.querySelectorAll(".data-gropdown");
+const dropdownsButtons = document.querySelectorAll(".data-gropdown-button");
+
+for (let dropdownsButton of dropdownsButtons) {
+  const dropdownsButtonAtr = dropdownsButton.getAttribute(
+    "data-gropdown-button"
+  );
+  dropdownsButton.addEventListener("click", function () {
+    for (let dropdown of dropdowns) {
+      const dropdownAtr = dropdown.getAttribute("data-gropdown");
+      if (dropdownsButtonAtr === dropdownAtr) {
+        dropdown.classList.toggle("none");
+      }
+    }
+  });
+}
+
+// MOBILE NAV
+
+const burgerIcon = document.getElementById("burgerIcon");
+const mobileMenu = document.querySelector(".header__row_nav_mobile");
+const overlay = document.querySelector(".overlay");
+
+burgerIcon.addEventListener("click", function (e) {
+  e.preventDefault()
+  mobileMenu.classList.toggle("show");
+  overlay.classList.toggle("show");
 });
